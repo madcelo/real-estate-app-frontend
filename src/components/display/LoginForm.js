bd58925f9b8367b35ce8acf4ac432b5b7441a2ab
Marcelo Di Jorge <marcelobdij@gmail.com>
2023-05-09 00:55:29 -0300

    feat: add Login page and form; add auth by JWT

---
+++ src/components/display/LoginForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";   
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("/api/auth/login", { email, password });
+      const { token } = response.data;
+
+      localStorage.setItem("authToken", token);
+      navigate("/admin");
+    } catch (error) {
+      setError("Invalid email or password");
+      setTimeout(() => setError(""), 5000);
+    }
+  };
+
+  return (
+    <div className="login-form-container">
+      {error && <span className="error-message">{error}</span>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          minLength="6"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "../display/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <h2>Login</h2>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
